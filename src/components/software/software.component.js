dc2bb0df1aad962c2b802d7921209eb42ff4213a
Jessy Hu <jesshuy12@gmail.com>
2020-02-08 16:58:15 -0500

    software page final fixes

--- src/components/software/software.component.js
+++ src/components/software/software.component.js
@@ -41,12 +41,82 @@ const SoftwareInfo = () => (
             </div>
             <div className="item">
                 <p className="item-title">BACKEND</p>
+                <span className="contents">
+                    <div className="content-separator">
+                        <p className="content">01</p>
+                        <p className="content-name">Ruby</p>
+                    </div>
+                    <div className="content-separator">
+                        <p className="content">02</p>
+                        <p className="content-name">Ruby on Rails</p>
+                    </div>
+                    <div className="content-separator">
+                        <p className="content">03</p>
+                        <p className="content-name">PostgreSQL</p>
+                    </div>
+                    <div className="content-separator">
+                        <p className="content">04</p>
+                        <p className="content-name">MySQL</p>
+                    </div>
+                    <div className="content-separator">
+                        <p className="content">05</p>
+                        <p className="content-name">Firebase</p>
+                    </div>
+                </span>
             </div>
             <div className="item">
                 <p className="item-title">OTHER</p>
+                <span className="contents">
+                    <div className="content-separator">
+                        <p className="content">01</p>
+                        <p className="content-name">Git / Github</p>
+                    </div>
+                    <div className="content-separator">
+                        <p className="content">02</p>
+                        <p className="content-name">Heroku</p>
+                    </div>
+                    <div className="content-separator">
+                        <p className="content">03</p>
+                        <p className="content-name">JSON APIs</p>
+                    </div>
+                    <div className="content-separator">
+                        <p className="content">04</p>
+                        <p className="content-name">API Integrations</p>
+                    </div>
+                    <div className="content-separator">
+                        <p className="content">05</p>
+                        <p className="content-name">MVC Frameworks</p>
+                    </div>
+                    <div className="content-separator">
+                        <p className="content">06</p>
+                        <p className="content-name">UX/UI</p>
+                    </div>
+                </span>
             </div>
             <div className="item">
                 <p className="item-title">SOFTWARES</p>
+                <span className="contents">
+                    <div className="content-separator">
+                        <p className="content">01</p>
+                        <p className="content-name">MS Office 365</p>
+                    </div>
+                    <div className="content-separator">
+                        <p className="content">02</p>
+                        <p className="content-name">MS Project</p>
+                    </div>
+                    <div className="content-separator">
+                        <p className="content">03</p>
+                        <p className="content-name">AutoCAD</p>
+                    </div>
+                    <div className="content-separator">
+                        <p className="content">04</p>
+                        <p className="content-name">MATLAB</p>
+                    </div>
+                    <div className="content-separator">
+                        <p className="content">05</p>
+                        <p className="content-name">Adobe Lightroom</p>
+                    </div>
+                </span>
             </div>
         </div>
     </div>
